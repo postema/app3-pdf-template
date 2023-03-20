0df069b25e2031eb25bdb1c4a38a84a81ea6094e
postema <pj.postema@gmail.com>
2023-03-20 15:20:25 +0100

    set multiple pages

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # set header
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100, 100, 100)
-    pdf.line(10, 21, 200, 21)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+    # set footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=8, txt=row["Topic"], align="R", ln=1)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        # set footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=8, txt=row["Topic"], align="R", ln=1)
 
 pdf.output("output.pdf")
